43809bcdf822d3280a9c1b30f4e35f555cd07dfe
joel <joel.westerlund@hotmail.com>
2019-02-06 17:09:05 +0100

    First version of the app

---
+++ InstaChat/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Alert,Button,StyleSheet, Text, View} from 'react-native';
+
+export default class App extends React.Component {
+
+    onPressCreate() {
+        Alert.alert('This should create a new chatroom')
+    }
+
+    onPressJoin() {
+        Alert.alert('This should join a chatroom')
+    }
+
+    onPressOngoing() {
+        Alert.alert('This should change the view!')
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this.onPressCreate}
+                        title="Create"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this.onPressJoin}
+                        title="Join"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        onPress={this.onPressOngoing}
+                        title="Ongoing"
+                    />
+                </View>
+            </View>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        margin: 20
+    }
+});
