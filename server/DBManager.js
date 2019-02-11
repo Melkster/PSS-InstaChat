6be934602a08545a22d02a09010cd42fb46aca77
Redhotsmasher <Red_hot_smasher@yahoo.com>
2019-02-11 23:37:27 +0100

    Implemented createUser(), which generates a random user ID (currently 32 bytes) and adds it to the global user table, generating a new id in case of a collision. Also edited package.json to add dependencies (not sure exactly which version of sqlite3 is required so left that blank for now) and add myself as an author.

--- server/DBManager.js
+++ server/DBManager.js
@@ -1,6 +1,7 @@
 /** Interface to the database which manages chats and users */
 const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
+const crypto = require('crypto');
 
 var globaldb;
 
@@ -25,9 +26,9 @@ class DBManager {
         
         if(firstboot == true) {
             globaldb.serialize(() => {
-                globaldb.run('CREATE TABLE GlobalUsers(USERHASH INT     PRIMARY KEY      NOT NULL        );')      })
+                globaldb.run('CREATE TABLE GlobalUsers(USERHASH INT     PRIMARY KEY      NOT NULL        );')      
                 .run('CREATE TABLE GlobalChats(CHATHASH INT     PRIMARY KEY      NOT NULL);')
-                .run('INSERT INTO GlobalUsers (USERHASH) VALUES(0);');
+                .run('INSERT INTO GlobalUsers (USERHASH) VALUES(0);')});
         }
 
         return true;
@@ -36,7 +37,7 @@ class DBManager {
     deinitDatabase() {
         // SLQ queries to deinitialize global database
         // return: true if deinitialized successfully, otherwise false
-        // TODO
+        // low priority
     }
     
     createChat(chatName) {
@@ -52,13 +53,30 @@ class DBManager {
     }
 
     createUser() {
-        // SQL query to create global user
-        // return: userID if user was added successfully, otherwise false
-        // TODO
+        var success = false;
+        var userid;
+        var tries = 0;
+        while(success == false) {
+            success = true;
+            userid = DBManager.sRandomBigValue();
+            globaldb.run('INSERT INTO GlobalUsers (USERHASH) VALUES(?);', [userid], (err) => {
+                if(err) {
+                    success = false;
+                    if(tries > 16) {
+                        return false;
+                    }
+                    tries++;
+                } 
+            });
+        }
+        return userid;
+        //    globaldb.run('INSERT INTO GlobalUsers (USERHASH) VALUES(?);', [userhash]);
     }
 
     deleteUser(userID) {
         // SQL query to delete global user `userID`
+        // return: true if deleted successfully, otherwise false
+        // low priority
     }
 
     addUser(userID, userName, chatID) {
@@ -69,7 +87,7 @@ class DBManager {
     
     removeUser(userID, chatID) {
         // SQL query to remove user with `userID` from chat with `chatID`
-        // @return: true if user was removed successfully, otherwise false
+        // return: true if user was removed successfully, otherwise false
         // low priority
     }
 
@@ -88,6 +106,25 @@ class DBManager {
         // SQL query to retreive chat history for chat with `chatID`
         // return: list of messages for chat with `chatID`
         // TODO
+    }
+
+    /* Utility functions below */
+
+    static sRandomBigValue() {
+        var array = crypto.randomBytes(32);
+        var stringarray = [];
+        for (var i = 0; i < array.length; i++) { 
+            stringarray.push(DBManager.U8ToHexString(array[i]));
+        }
+        return stringarray.join("");
+    }
+
+    static U8ToHexString(byte) {
+        var string = "";
+        var hinybble = byte >> 4;
+        var lonybble = byte - (hinybble << 4);
+        string = hinybble.toString(16);
+        return string.concat(lonybble.toString(16));
     }
 }
 
