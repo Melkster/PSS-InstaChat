ad87e80956e7561ff44e08bbf7b18de888396ecf
joel <joel.westerlund@hotmail.com>
2019-02-11 13:41:26 +0100

    App navigation and text input now possible

--- InstaChat/App.js
+++ InstaChat/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
-import {Alert,Button,StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import { Alert, Button, View, Text, TextInput, TouchableOpacity} from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default class App extends React.Component {
+const styles = ({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        margin: 20
+    },
+    submitButton: {
+        backgroundColor: '#7a42f4',
+        padding: 10,
+        margin: 15,
+        height: 40,
+    },
+    submitButtonText:{
+        color: 'white'
+    }
+});
+
+class HomeScreen extends React.Component {
 
     onPressCreate() {
         Alert.alert('This should create a chatroom')
@@ -9,10 +31,6 @@ export default class App extends React.Component {
 
     onPressJoin() {
         Alert.alert('This should join a chatroom')
-    }
-
-    onPressOngoing() {
-        Alert.alert('This should change the view!')
     }
 
     render() {
@@ -32,8 +50,8 @@ export default class App extends React.Component {
                 </View>
                 <View style={styles.buttonContainer}>
                     <Button
-                        onPress={this.onPressOngoing}
-                        title="Ongoing"
+                        title="Go to Chats"
+                        onPress={() => this.props.navigation.navigate('Chats')}
                     />
                 </View>
             </View>
@@ -42,14 +60,48 @@ export default class App extends React.Component {
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonContainer: {
-        margin: 20
+class ChatScreen extends React.Component {
+    submitMessage() {
+       Alert.alert('Sending message to server');
     }
-});
+
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <TextInput
+                    style={{height: 40}}
+                    placeholder="Type your message!"
+                />
+                <TouchableOpacity
+                    style = {styles.submitButton}
+                    onPress = {this.submitMessage}
+                >
+                    <Text style = {styles.submitButtonText}> Submit </Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+/*function add(a, b) {
+        return a + b;
+}
+const add_lambda = (a, b) => a + b;*/
+
+const AppNavigator = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Chats: ChatScreen
+    },
+    {
+        initialRouteName: "Home"
+    }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+    render() {
+        return <AppContainer />;
+    }
+}
