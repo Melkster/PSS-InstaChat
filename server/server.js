48005982f04d0fac8e3790bdc8b853f67a54784f
Melker Österberg <melkerosterberg@gmail.com>
2019-02-14 14:07:27 +0100

    Handle users going online, checking for/creating their userID and joining chat rooms

---
+++ server/server.js
@@ -0,0 +1,73 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const DBManager = require("./DBManager.js");
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+const database = new DBManager();
+database.initDatabase();
+
+io.on("connection", socket => {
+    // TODO: redownload history when user enters a chat
+    // TODO: add time stamp to messages when received
+
+    console.log(`A user connected, socket ID ${socket.id}`);
+
+    socket.on("identificaiton", (userID, chatIDs) => {
+        if (userID == null) {
+            userID = DBManager.sRandomBigValue(); // generate new userID here
+            socket.emit("identificaiton", userID);
+            // TODO: should users be saved in the database if they haven't joined any chat room?
+        } else {
+            if (chatIDs != null && chatIDs.length > 0) {
+                for (chatID of chatIDs) {
+                    socket.join(chatID);
+                }
+            }
+        }
+    });
+
+    socket.on("joinChat", (userID, userName, chatID) => {
+        result = database.addUser(userID, userName, chatID);
+        if (result == false) {
+            socket.emit("err", `Could not join chat with chat ID "${chatID}"`);
+        } else {
+            socket.join(chatID);
+            console.log(`User with userID ${userID} joined chat ${chatID}`);
+        }
+    });
+
+    socket.on("leaveChat", () => {
+        // TODO
+    });
+
+    socket.on("createChat", chatName => {
+        chatID = database.createChat(chatName);
+        if (chatID == false) {
+            socket.emit("err", `Could not create chat "${chatName}"`);
+        } else {
+            socket.emit("createChat", chatID);
+        }
+    });
+
+    // Use the following to transmit the message:
+    // io.to(data.chatID).emit("message", data);
+    socket.on("message", data => {
+        data.time = new Date(); // Adds time received to message
+        messages.push(data);
+        io.sockets.emit("message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`A user disconnected, socket ID ${socket.id}`);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Listening on localhost:${port}`);
+});
