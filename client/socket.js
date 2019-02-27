eb861bc94071414f97c0780d5e9c9050e9771b7d
Melkster <melkerosterberg@gmail.com>
2019-02-27 22:08:34 +0100

    Only alert once if client can't connect to server (#30)
    * Only alert once if client can't connect to server
    
    * Alert if the the client reconnects to a previously disconnected server.

--- client/socket.js
+++ client/socket.js
@@ -5,10 +5,22 @@ import {
 
 socket = io("http://192.168.0.104:3000/"); // You need to change this to your server ip
 
+var connect_error = false; // Ensures that an error connecting only is alerted once
+
 // Alerts the user if they can't connect to the server
 socket.on("connect_error", error => {
-    Alert.alert("Couldn't connect to server");
+    if (connect_error == false) {
+        Alert.alert("Couldn't connect to server");
+        connect_error = true;
+    }
 });
+
+socket.on("connect", () => {
+    if (connect_error == true) {
+        Alert.alert("Reconnected with server");
+        connect_error = false;
+    }
+})
 
 // Error event handler, prints an Alert with the provided message
 socket.on("err", data => {
