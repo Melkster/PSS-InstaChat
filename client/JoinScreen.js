ebd7256f836576ef08edf39e9c2fbf8e66297918
swc1326 <shang1326@hotmail.com>
2019-03-01 11:59:28 +0100

    Update JoinScreen.js to connect to server.

--- client/JoinScreen.js
+++ client/JoinScreen.js
@@ -5,9 +5,39 @@ import {
 import styles from './styles';
 
 class JoinScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentState: this.props.navigation.getParam('currentState', 'unknown'),
+            chatID: '',
+            nickname: '',
+        };
+        socket.on("joinChat", this.handleJoinChat);
+    }
+
+    handleCreateChat = (name) => {
+        this.props.navigation.navigate('Chatroom', {
+            currentState: this.state.currentState,
+            chatID: this.state.chatID,
+            chatName: name,
+            nickname: this.state.nickname
+        })
+    };
+
     onJoinButtonPressed() {
-        //should push user information to the server, including username, userID
-        Alert.alert('Join a chatroom!')
+//        var chatRoom = {
+//            name: '',
+//            chatID: '',
+//            nickname: '',
+//        };
+//        chatRoom = JSON.parse(AsyncStorage.getItem('chats'));
+//        let userID = this.state.currentState.userID;
+//        let username = chatRoom.nickname;
+//        //let chatID = chatRoom.chatID;
+//        this.setState((prevState) => {
+//            return {nickname: username,};
+//        });
+        socket.emit("joinChat", this.state.currentState.userID, this.state.currentState.username, this.state.chatID);
     }
 
     render() {
@@ -15,7 +45,8 @@ class JoinScreen extends React.Component {
             <View style={styles.createScreenView}>
                 <TextInput
                     style={styles.chatRoomName}
-                    placeholder="Enter Chatroom Name"
+                    placeholder="Enter Chatroom ID"
+                    onChangeText={(text) => this.setState({chatID: text})}
                 />
                 <TouchableHighlight
                     style={styles.chatRoomNameSubmit}
