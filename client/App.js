79a75e0ff4eda140401d7ea04a6226a8789b4dfe
Nygards <axel949@hotmail.com>
2019-03-05 20:38:57 +0100

    Implementing new design

--- client/App.js
+++ client/App.js
@@ -1,17 +1,18 @@
-import React from "react";
-import {
-    AsyncStorage, Alert, Button, Keyboard, TouchableHighlight, View,
-    StyleSheet, Text, TextInput, TouchableOpacity, FlatList, KeyboardAvoidingView
-} from "react-native";
-import {createStackNavigator, createAppContainer} from "react-navigation";
-
-import ChatSelect from './ChatSelect';
-import JoinScreen from './JoinScreen';
-import ChatScreen from './ChatScreen';
-import InfoScreen from './InfoScreen';
-import CreateScreen from './CreateScreen';
-import socket from './socket';
-import styles from './styles';
+import React, { Component } from "react";
+import { AsyncStorage, Alert, Keyboard, TouchableHighlight, View, StyleSheet, TextInput, TouchableOpacity, FlatList, KeyboardAvoidingView } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, ActionSheet, StyleProvider } from "native-base";
+import ChatSelect from "./ChatSelect";
+import JoinScreen from "./JoinScreen";
+import ChatScreen from "./ChatScreen";
+import InfoScreen from "./InfoScreen";
+import CreateScreen from "./CreateScreen";
+import socket from "./socket";
+import styles from "./styles";
+import { Font } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import getTheme from './native-base-theme/components';
+import material from "./native-base-theme/variables/material";
 
 // Ignores yellow warnings, showed up when going to view with websockets.
 console.ignoredYellowBox = ["Remote debugger"];
@@ -21,116 +22,179 @@ YellowBox.ignoreWarnings([
 ]);
 
 class HomeScreen extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            'name': null,
-            'userID': null, // the server should give this
-            'chats': [] // {'name': 'BestChat', 'chatID': 'ABC123'}
+            name: null,
+            userID: null, // the server should give this
+            chats: [], // {'name': 'BestChat', 'chatID': 'ABC123'}
+            loading: true
         };
 
-
-        socket.on("reconnect", (attempNumber) => {
-            console.log('Reconnected with server');
-            socket.emit('identification', this.state.userID, this.state.chats.map(obj => obj.chatID));
+        socket.on("reconnect", attempNumber => {
+            console.log("Reconnected with server");
+            socket.emit("identification", this.state.userID, this.state.chats.map(obj => obj.chatID));
         });
-
     }
 
     /* This section is performed every time the application starts, it tries to load saved information
     and if that information is not found, the information is requested from the server */
-    componentDidMount = () => {
-        AsyncStorage.getItem('name').then((value) => this.setState({'name': value}));
-        AsyncStorage.getItem('chats').then((value) => {
-            if(value !== null) {
-                this.setState({'chats': JSON.parse(value)});
+    async componentWillMount() {
+        await Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            ...Ionicons.font
+        });
+        this.setState({ loading: false });
+    }
+    async componentDidMount() {
+        AsyncStorage.getItem("name").then(value => this.setState({ name: value }));
+        AsyncStorage.getItem("chats").then(value => {
+            if (value !== null) {
+                this.setState({ chats: JSON.parse(value) });
             }
         });
-        AsyncStorage.getItem('userID').then((value) => {
-            socket.emit('identification', value, this.state.chats.map(obj => obj.chatID));
-            socket.on('identification', (value) => {
+        AsyncStorage.getItem("userID").then(value => {
+            socket.emit("identification", value, this.state.chats.map(obj => obj.chatID));
+            socket.on("identification", value => {
                 if (value == null) {
-                    alert('Server returned. null');
+                    alert("Server returned. null");
                 } else {
-                    alert('Received ' + value + ' from server');
-                    AsyncStorage.setItem('userID', value);
-                    this.setState({'userID': value});
+                    alert("Received " + value + " from server");
+                    AsyncStorage.setItem("userID", value);
+                    this.setState({ userID: value });
                 }
             });
-            this.setState({'userID': value});
+            this.setState({ userID: value });
         });
-    };
+    }
 
     /* This function is sent to the infoscreen, there you can choose to forget the current stored info*/
     removeInfo = () => {
-        AsyncStorage.removeItem('name');
-        AsyncStorage.removeItem('userID');
-        AsyncStorage.removeItem('chats');
+        AsyncStorage.removeItem("name");
+        AsyncStorage.removeItem("userID");
+        AsyncStorage.removeItem("chats");
         this.state.name = null;
         this.state.userID = null;
         this.state.chats = [];
-        alert('Please restart the app');
+        alert("Please restart the app");
     };
 
-    setName = (value) => {
+    setName = value => {
         // An example of how information can be stored in client
-        AsyncStorage.setItem('name', value);
-        this.setState({'name': value});
+        AsyncStorage.setItem("name", value);
+        this.setState({ name: value });
     };
 
     render() {
+        if (this.state.loading) {
+            return null;
+        }
         return (
+            // <StyleProvider style={getTheme(material)}>
+                <Container style={styles.container}>
+                    <Header>
+                        <Body>
+                            <Title style={{ alignSelf: "center" }}>InstaChat</Title>
+                        </Body>
+                        {/* <Text>InstaChat</Text> */}
+                    </Header>
+                    <Content>
+                        <Button
+                            Large
+                            primary
+                            style={{ alignSelf: "center" }}
+                            onPress={() =>
+                                this.props.navigation.navigate("Create", {
+                                    currentState: this.state
+                                })
+                            }
+                        >
+                            <Text>Create</Text>
+                        </Button>
+                        <Button
+                            style={{ alignSelf: "center" }}
+                            onPress={() =>
+                                this.props.navigation.navigate("Join", {
+                                    currentState: this.state
+                                })
+                            }
+                        >
+                            <Text>Join</Text>
+                        </Button>
 
-            <View style={styles.container}>
-                <Text style={styles.headline}>InstaChat</Text>
-                <View style={styles.buttonContainer}>
-                    <TextInput style={styles.textInput}
-                               placeholder='Enter your nickname'
-                               onChangeText={this.setName}
-                               onSubmitEditing={Keyboard.dismiss}
-                    />
-                    <Text>
-                        Your nickname is: {this.state.name}
-                    </Text>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        onPress={() => this.props.navigation.navigate('Create', {
-                            currentState: this.state })
-                        }
-                        title="Create"
-                    />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title="Join"
-                        onPress={() => this.props.navigation.navigate('Join', {
-                            currentState: this.state })
-                        }
-                    />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title="Go to Chats"
-                        onPress={() => this.props.navigation.navigate('Chats', {
-                            currentState: this.state })
-                        }
-                    />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <Button
-                        title="Info"
-                        onPress={() => this.props.navigation.navigate('Info', {
-                            currentState: this.state, removeFunc: this.removeInfo})
-                        }
-                    />
-                </View>
-            </View>
-
+                        <Button
+                            style={{ alignSelf: "center" }}
+                            onPress={() =>
+                                this.props.navigation.navigate("Chats", {
+                                    currentState: this.state
+                                })
+                            }
+                        >
+                            <Text>Chats</Text>
+                        </Button>
+                        <Button
+                            style={{ alignSelf: "center" }}
+                            onPress={() =>
+                                this.props.navigation.navigate("Info", {
+                                    currentState: this.state
+                                })
+                            }
+                        >
+                            <Text>Info</Text>
+                        </Button>
+                    </Content>
+                </Container>
+            // </StyleProvider>
         );
     }
 }
+
+// <View style={styles.container}>
+//     <Text style={styles.headline}>InstaChat</Text>
+//     <View style={styles.buttonContainer}>
+//         <TextInput style={styles.textInput}
+//                 placeholder='Enter your nickname'
+//                 onChangeText={this.setName}
+//                 onSubmitEditing={Keyboard.dismiss}
+//         />
+//         <Text>
+//             Your nickname is: {this.state.name}
+//         </Text>
+//     </View>
+//     <View style={styles.buttonContainer}>
+//         <Button
+//             onPress={() => this.props.navigation.navigate('Create', {
+//                 currentState: this.state })
+//             }
+//             title="Create"
+//         />
+//     </View>
+//     <View style={styles.buttonContainer}>
+//         <Button
+//             title="Join"
+//             onPress={() => this.props.navigation.navigate('Join', {
+//                 currentState: this.state })
+//             }
+//         />
+//     </View>
+//     <View style={styles.buttonContainer}>
+//         <Button
+//             title="Go to Chats"
+//             onPress={() => this.props.navigation.navigate('Chats', {
+//                 currentState: this.state })
+//             }
+//         />
+//     </View>
+//     <View style={styles.buttonContainer}>
+//         <Button
+//             title="Info"
+//             onPress={() => this.props.navigation.navigate('Info', {
+//                 currentState: this.state, removeFunc: this.removeInfo})
+//             }
+//         />
+//     </View>
+// </View>
 
 const AppNavigator = createStackNavigator(
     {
@@ -142,6 +206,7 @@ const AppNavigator = createStackNavigator(
         Info: InfoScreen
     },
     {
+        headerMode: "none",
         initialRouteName: "Home"
     }
 );
@@ -150,6 +215,6 @@ const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
     render() {
-        return <AppContainer/>;
+        return <AppContainer />;
     }
 }
--- client/styles.js
+++ client/styles.js
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
+        backgroundColor: '#80dfff'
     },
     buttonContainer: {
         margin: 20
