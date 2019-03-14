7af4b61aa8b62561dd23a55d16d32d44ae037d4f
swc1326 <shang1326@hotmail.com>
2019-03-14 23:57:00 +0100

    User can quit from a chatroom.(Only client side right now.) (#37)

--- client/ChatSelect.js
+++ client/ChatSelect.js
@@ -1,40 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
 import {
-    Button, View, Text, ScrollView
+    View, FlatList,
 } from "react-native";
 import styles from './styles';
-import chatRoomsList from './ChatRoomsList';
+import SelectItem from './SelectItem.js';
 
 class ChatSelect extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            props: this.props,
+            currentState: this.props.navigation.getParam('currentState', 'unknown'),
+            deletedRowKey: null,
+        };
+    }
+
+    keyExtractor = (item, index) => index.toString();
+
+    //Refresh FlatList by changing a state value
+    refreshFlatList = (deletedKey) => {
+        this.setState((prevState) => {
+            return {deletedRowKey: deletedKey};
+        });
     }
 
     render() {
-        const { navigation } = this.props;
-        const state = navigation.getParam('currentState', 'unknown');
-
         return (
-            <View>
-                <Text style={styles.headline}>Chatselect</Text>
-                <ScrollView>
-                    {
-                        state.chats.map((item, index) => (
-                            <View key = {item.chatID} style={styles.buttonContainer}>
-                                <Button
-                                    title={item.name}
-                                    onPress={() => this.props.navigation.navigate('Chatroom', {
-                                        currentState: state,
-                                        chatID: item.chatID,
-                                        nickname: item.nickname,
-                                        chatName: item.name})
-                                    }
-                                />
-                            </View>
-
-                        ))
-                    }
-                </ScrollView>
+            <View style={{flex: 1,}} >
+                <FlatList
+                    data = {this.state.currentState.chats}
+                    renderItem = { ({item, index}) => {
+                        return (
+                            <SelectItem
+                                item={item}
+                                index={index}
+                                currentState={this.state.currentState}
+                                props={this.state.props}
+                                parentList={this}
+                            >
+                            </SelectItem>
+                        );
+                    }}
+                    keyExtractor={this.keyExtractor}
+                >
+                </FlatList>
             </View>
         );
     }
